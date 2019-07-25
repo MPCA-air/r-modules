7c0bf12f166db5bc5bc674541195581abc2dd034
air <dKvale@users.noreply.github.com>
2019-07-25 15:43:58 -0500

    Update add_md.R

--- r/add_md.R
+++ r/add_md.R
@@ -1,9 +1,14 @@
 add_md <- function(file) {
 
-  out <- paste("```{r goodmorns, results='asis', echo=F}",
-               paste0("cat(readLines(", file, "))"),
-               "```\n", sep = "\n\n")
-  cat(out)
+  out <- ""
   
+  out <- tryCatch(paste0(out, "cat(readLines(", file, "))"), error = function(e) NA)
+  
+  if(!is.na(out)) {
+    out <- paste("```{r goodmorns, results='asis', echo=F}",
+                 paste0("cat(readLines(", file, "))"),
+                 "```\n", sep = "\n\n")
+    cat(out)
+  }               
   return(out)
 }
