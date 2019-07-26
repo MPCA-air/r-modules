e0c2266a0a08a92228da7ad5b101852ab14c828c
air <dKvale@users.noreply.github.com>
2019-07-26 17:23:57 -0500

    Update add_color_margin.R

--- R/add_color_margin.R
+++ R/add_color_margin.R
@@ -1,7 +1,7 @@
 knitr::knit_hooks$set(color = function(before, options, envir) {
     if (before) {
         ## code to be run before a chunk
-        return(paste0('<div style="margin-left: -18px; padding-left: 26px; border-left: solid 8px ', options$color, ';>'))
+        return(paste0('<div style="margin-left: -28px; padding-left: 32px; border-left: solid 4px ', options$color, ';">'))
     } else {
         ## code to be run after a chunk
         return("</div>")
