55f5c25a93134ff5df01f6b06e4b8d6e58184d2b
air <dKvale@users.noreply.github.com>
2019-07-25 19:17:43 -0500

    Update add_md.R

--- R/add_md.R
+++ R/add_md.R
@@ -1,6 +1,6 @@
 add_md <- function(file) {
 
-  out <- paste("```{r goodmorns, results='asis', echo=F}",
+  out <- paste("```{r, results='asis', echo=F}",
                paste0("cat(readLines(", file, "))"),
                "```\n", sep = "\n\n")
   cat(out)
