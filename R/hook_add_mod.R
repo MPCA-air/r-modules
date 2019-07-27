39e6d9454e7f177a2961ce41e7f1ab63dd9e2148
air <dKvale@users.noreply.github.com>
2019-07-27 10:52:28 -0500

    Update hook_add_mod.R

--- R/hook_add_mod.R
+++ R/hook_add_mod.R
@@ -6,7 +6,7 @@ knitr::knit_hooks$set(color = function(before, options, envir) {
         if (!is.null(options$color)) out <- paste0('<div style="padding: 12px; background-color: ', options$color, ';">\n')
         mod_url <- "https://raw.githubusercontent.com/MPCA-air/r-modules/master/modules/"
         out <- paste0(out, readLines(paste0(mod_url, "_whyr/itep.Rmd")))
-        return(paste(out, collapse = "\n"))
+        return(paste(out, collapse = ""))
     } else {
         ## code to be run after a chunk
         if (!is.null(options$color)) return("</div>")
