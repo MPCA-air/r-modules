309ea237b5d7db644308e04011c38a3df9c6505a
air <dKvale@users.noreply.github.com>
2019-07-27 11:09:01 -0500

    Update hook_add_mod.R

--- R/hook_add_mod.R
+++ R/hook_add_mod.R
@@ -3,10 +3,11 @@ knitr::knit_hooks$set(color = function(before, options, envir) {
     if (before) {
         ## code to be run before a chunk
         out <- ""
-        if (!is.null(options$color)) out <- paste0('<div style="padding: 12px; background-color: ', options$color, ';">\n')
+        if (!is.null(options$color)) out <- paste0('<div style="padding: 12px; background-color: ', options$color, ';">\n  ')
+        
         mod_url <- "https://raw.githubusercontent.com/MPCA-air/r-modules/master/modules/"
         out <- paste0(out, readLines(paste0(mod_url, "_whyr/itep.Rmd")))
-        return(paste(out, collapse = ""))
+        return(out)
     } else {
         ## code to be run after a chunk
         if (!is.null(options$color)) return("</div>")
