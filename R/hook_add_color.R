e86a2f9853edb9dc74d88c8fcff7f3cc268ac001
air <dKvale@users.noreply.github.com>
2019-07-27 10:18:14 -0500

    Update hook_add_color.R

--- R/hook_add_color.R
+++ R/hook_add_color.R
@@ -1,5 +1,5 @@
 knitr::knit_hooks$set(color = function(before, options, envir) {
-    if (!is.null(add_mod) {
+    if (!is.null(add_mod)) {
         if (before) {
             ## code to be run before a chunk
             return(paste0('<div style="padding: 18px; background-color: ', options$color, ';">'))
