6664751f57c6e4a42ebab9325ba57f1b6d5a9183
air <dKvale@users.noreply.github.com>
2019-07-28 10:14:34 -0500

    Update hook_add_mod.R

--- R/hook_add_mod.R
+++ R/hook_add_mod.R
@@ -7,7 +7,7 @@ knitr::knit_hooks$set(color = function(before, options, envir) {
         ## code to be run before a chunk
         out <- ""
         if (!is.null(options$color)) out <- paste0('<div style="padding: 16px; background-color: ', options$color, ';">\n\n')
-        out <- paste0(out, paste0(readLines(paste0(mod_url, "_whyr/itep.Rmd")), collapse = "\n"))
+        out <- paste0(out, paste0(readLines(paste0(mod_url, options$add_mod)), collapse = "\n"))
         return(out)
     } else {
         ## code to be run after a chunk
